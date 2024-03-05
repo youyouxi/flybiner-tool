3e0354686b8b3511f07d4d0ca33c0f50b7bd5bc2
陈建斌 <chenjb1287@fingard.com>
2024-03-05 20:11:57 +0800

    创建菜单

--- src/page/IndexPage.js
+++ src/page/IndexPage.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Button } from 'antd';
+import { AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
 
-const IndexPage: React.FC = () => (
-    <div className="App">
-        <Button type="primary">Button</Button>
-    </div>
-);
+function getItem(label, key, icon, children, type) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    };
+}
+
+const items = [
+    getItem('工作台', 'grp', null, [getItem('', '13', <SettingOutlined />), getItem('', '14',  <AppstoreOutlined />)], 'group'),
+];
+const IndexPage = () => {
+    const onClick = (e) => {
+        console.log('click ', e);
+    };
+    return (
+        <Menu
+            onClick={onClick}
+            style={{
+                width: 75,
+                height: '100vh'
+            }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            items={items}
+        />
+    );
+};
 
 export default IndexPage;
